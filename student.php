f71a30550eb45260a46e8d764f192a131723174c
Franze <diazfranzpeter@gmail.com>
2023-10-14 11:33:32 +0800

    Side Bar Added

---
+++ student.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instructor</title>
+    <?php
+    include 'sideBar-Style.php';
+    ?>
+</head>
+
+<body>
+    <div class="wrapper d-flex">
+        <?php
+        include 'sideBar.php';
+        ?>
+        <h1>Student Page</h1>
+    </div>
+
+</body>
+
+</html>
