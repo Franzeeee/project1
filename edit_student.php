d815587d2945e6f24ccc1f4cd61c3688b157c190
Franze <diazfranzpeter@gmail.com>
2023-10-19 08:58:09 +0800

    Updated Student Page

---
+++ edit_student.php
@@ -0,0 +1,70 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $student_id = $_GET['id'];
+    
+    // Fetch the student's current information from the database
+    $pdo = new PDO('mysql:host=localhost;dbname=project1', 'root');
+    $stmt = $pdo->prepare('SELECT * FROM students WHERE student_id = ?');
+    $stmt->execute([$student_id]);
+    $student = $stmt->fetch();
+    
+    if ($student) {
+        // Display the edit form with the current information
+        $lastname = $student['lastname'];
+        $firstname = $student['firstname'];
+        $student_id = $student['student_id'];
+    } else {
+        echo 'Student not found.';
+        // You can add a link to go back to the student list page
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Handle the form submission to update the student's information in the database
+    $student_id = $_POST['student_id'];
+    $new_lastname = $_POST['lastname'];
+    $new_firstname = $_POST['firstname'];
+    
+    $pdo = new PDO('mysql:host=localhost; dbname=project1', 'root');
+    $stmt = $pdo->prepare('UPDATE students SET lastname = ?, firstname = ? WHERE student_id = ?');
+    $result = $stmt->execute([$new_lastname, $new_firstname, $student_id]);
+    
+    if ($result) {
+        header('Location: student.php');
+        exit();
+    } else {
+        echo 'Error updating the student.';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Student</title>
+    <link rel="stylesheet" type="text/css" href="css/studentpage.css">
+    <?php
+    include 'sideBar-Style.php';
+    ?>
+</head>
+<body>
+    <div class="wrapper d-flex">
+        <!-- Sidebar Code -->
+        <?php include 'sideBar.php'; ?>
+        <!-- Sidebar Code -->
+        <!-- Content for Edit Student Page -->
+        <div class="content">
+            <h1>Edit Student</h1>
+            <form method="post" action="edit_student.php">
+                <input type="hidden" name="student_id" value="<?= $student_id ?>">
+                <label for="lastname">Lastname:</label>
+                <input type="text" id="lastname" name="lastname" value="<?= $lastname ?>" required>
+                <label for="firstname">Firstname:</label>
+                <input type="text" id="firstname" name="firstname" value="<?= $firstname ?>" required>
+                <button type="submit">Save Changes</button>
+            </form>    
+        </div>
+        <!-- Content for Edit Student Page -->
+    </div>
+</body>
+</html>
--- student.php
+++ student.php
@@ -4,33 +4,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Instructor</title>
-    <style>
-        .student {
-            background-color: #fff;
-            color: black;
-        }
-
-        .student img {
-            filter: brightness(0);
-        }
-    </style>
+    <title>Student Page</title>
+    <link rel="stylesheet" type="text/css" href="css/studentpage.css">
+    <?php include 'sideBar-Style.php'; ?>
 </head>
 
 <body>
     <div class="wrapper d-flex">
-        <!-- Side Bar Code -->
-        <?php
-        include 'sideBar.php';
-        ?>
-        <!-- Side Bar Code -->
+        <!-- Sidebar Code -->
+        <?php include 'sideBar.php'; ?>
+        <!-- Sidebar Code -->
+        <!-- Content for Students Page -->
+        <div class="content">
+            <h1>Students Page</h1>
 
-        <!-- You can start coding bellow -->
-        <h1>Student Page</h1>
+            <!-- List of Students -->
+            <table>
+                <tr>
+                    <th>Lastname</th>
+                    <th>Firstname</th>
+                    <th>Student ID</th>
+                </tr>
+                <?php
+                $pdo = new PDO('mysql:host=localhost; dbname=project1', 'root');
+                $stmt = $pdo->query('SELECT * FROM students');
+                while ($row = $stmt->fetch()) {
+                    echo "<tr>";
+                    echo "<td>{$row['lastname']}</td>";
+                    echo "<td>{$row['firstname']}</td>";
+                    echo "<td>{$row['student_id']}</td>";
+                    echo "<td>
+                            <button><a href='student_subjects.php?id={$row['student_id']}'>View Subjects</a></button>
+                            <button><a href='edit_student.php?id={$row['student_id']}'>Edit</a></button>
+                            <button><a href='delete_student.php?id={$row['student_id']}'>Delete</a></button>
+                          </td>";
+                    echo "</tr>";
+                }
+                ?>
+            </table>
 
+            <!-- Button to Add Student -->
+            <a href="add_student.php" class="add-student-button">Add Student</a>
+        </div>
+        <!-- Content for Students Page -->
 
     </div>
-
 </body>
 
 </html>
