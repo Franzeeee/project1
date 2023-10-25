cf648fbdddda4665cb06550cb3aa9bdee53803cd
Vincent Cormero <vcormero48@gmail.com>
2023-10-25 12:59:02 +0800

    login and register is integrated

---
+++ php-practice/lFunction.php
@@ -0,0 +1,26 @@
+<?php
+require_once("database_connector.php");
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $find = $conn->query("SELECT * FROM `registration` WHERE username = '$username'");
+
+    if ($find->num_rows > 0) {
+        $data = $find->fetch_array();
+
+        if (password_verify($password, $data['password'])) {
+            $_SESSION['username'] = $username;
+
+            header("Location: ../subject.php");
+        } else {
+            echo "<script> alert('Password is incorrect!');</script>";
+        }
+    } else if (!empty($username)) {
+        echo "<script> alert('Username does not exist in our database!');</script>";
+    }
+}
