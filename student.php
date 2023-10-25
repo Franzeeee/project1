ed2aa24ad4f9fa195ebaad14d1cd0fdd5c6b5c7f
GregAnguren <145722505+GregAnguren@users.noreply.github.com>
2023-10-25 16:09:36 +0800

    Update student.php
    Changed design

--- student.php
+++ student.php
@@ -19,12 +19,17 @@
             <h1>Students Page</h1>
 
             <!-- List of Students -->
-            <table>
+            <table class="table">
+                <thead>
                 <tr>
                     <th>Lastname</th>
                     <th>Firstname</th>
                     <th>Student ID</th>
+                    <th>Action</th>
                 </tr>
+                </thead>
+
+                <tbody class="table-group-divider">
                 <?php
                 $pdo = new PDO('mysql:host=localhost; dbname=project1', 'root');
                 $stmt = $pdo->query('SELECT * FROM students');
@@ -34,13 +39,14 @@
                     echo "<td>{$row['firstname']}</td>";
                     echo "<td>{$row['student_id']}</td>";
                     echo "<td>
-                            <button><a href='student_subjects.php?id={$row['student_id']}'>View Subjects</a></button>
-                            <button><a href='edit_student.php?id={$row['student_id']}'>Edit</a></button>
-                            <button><a href='delete_student.php?id={$row['student_id']}'>Delete</a></button>
+                            <button class='btn btn-primary'><a style='color:white' href='student_subjects.php?id={$row['id']}'>View Subjects</a></button>
+                            <button class='btn btn-success'><a style='color:white' href='edit_student.php?id={$row['student_id']}'>Edit</a></button>
+                            <button class='btn btn-danger'><a style='color:white' href='delete_student.php?id={$row['student_id']}'>Delete</a></button>
                           </td>";
                     echo "</tr>";
                 }
                 ?>
+                </tbody>
             </table>
 
             <!-- Button to Add Student -->
